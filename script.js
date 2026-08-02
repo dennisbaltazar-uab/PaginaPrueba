5134aa00406c03491d29d9b5e32e6269f51d2571
Dennis Alexander Baltazar Condori <dennis.baltazar@uab.edu.bo>
2026-08-02 18:17:08 -0400

    claro osuro arreglado

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const botonTema = document.querySelector("#tema-btn");
-
+const body = document.body;
 function cambiarTema() {
-    document.body.classList.toggle("oscuro");
-    if (document.body.classList.contains("oscuro")) {
-        botonTema.textContent = "☀️ Modo Claro";
+    body.classList.toggle("oscuro");
+    if (body.classList.contains("oscuro")) {
+        botonTema.textContent = "Modo Claro";
     } else {
-        botonTema.textContent = "🌙 Modo Oscuro";
+        botonTema.textContent = "Modo Oscuro";
     }
 }
-
 botonTema.addEventListener("click", cambiarTema);
+console.log("Botón encontrado:", botonTema);
